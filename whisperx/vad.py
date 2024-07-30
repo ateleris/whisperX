9800cfdb13402bfa781eac8bfea80ccc2e51b5d7
Orell Bühler <orell.buehler@students.fhnw.ch>
2024-07-30 10:20:47 +0200

    remove unused parts

--- whisperx/vad.py
+++ whisperx/vad.py
@@ -1,6 +1,4 @@
-import hashlib
 import os
-import urllib
 from typing import Callable, Optional, Text, Union
 
 import numpy as np
@@ -11,13 +9,12 @@ from pyannote.audio.core.io import AudioFile
 from pyannote.audio.pipelines import VoiceActivityDetection
 from pyannote.audio.pipelines.utils import PipelineModel
 from pyannote.core import Annotation, Segment, SlidingWindowFeature
-from tqdm import tqdm
 
 from .diarize import Segment as SegmentX
 
 VAD_SEGMENTATION_URL = "https://whisperx.s3.eu-west-2.amazonaws.com/model_weights/segmentation/0b5b3216d60a2d32fc086b47ea8c67589aaeb26b7e07fcbe620d6d0b83e209ea/pytorch_model.bin"
 
-def load_vad_model(device, vad_onset=0.500, vad_offset=0.363, use_auth_token=None, model_fp=None):
+def load_vad_model(device, vad_onset=0.500, vad_offset=0.363, model_fp=None):
     model_dir = torch.hub._get_torch_home()
     os.makedirs(model_dir, exist_ok = True)
     if model_fp is None:
@@ -25,24 +22,10 @@ def load_vad_model(device, vad_onset=0.500, vad_offset=0.363, use_auth_token=Non
     if os.path.exists(model_fp) and not os.path.isfile(model_fp):
         raise RuntimeError(f"{model_fp} exists and is not a regular file")
 
-    if not os.path.isfile(model_fp):
-        with urllib.request.urlopen(VAD_SEGMENTATION_URL) as source, open(model_fp, "wb") as output:
-            with tqdm(
-                total=int(source.info().get("Content-Length")),
-                ncols=80,
-                unit="iB",
-                unit_scale=True,
-                unit_divisor=1024,
-            ) as loop:
-                while True:
-                    buffer = source.read(8192)
-                    if not buffer:
-                        break
+    if not os.path.exists(model_fp):
+        raise RuntimeError(f"{model_fp} does not exist")
 
-                    output.write(buffer)
-                    loop.update(len(buffer))
-
-    vad_model = Model.from_pretrained(model_fp, use_auth_token=use_auth_token)
+    vad_model = Model.from_pretrained(model_fp, use_auth_token=None)
     hyperparameters = {"onset": vad_onset, 
                     "offset": vad_offset,
                     "min_duration_on": 0.1,
