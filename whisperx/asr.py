a36622e63842061433d627cff3d80ddcdeada4f9
Orell Bühler <orell.buehler@ateleris.ch>
2024-10-28 17:33:56 +0100

    add hotwords argument from latest faster-whisper

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -402,6 +402,7 @@ def load_model(
         "max_new_tokens": None,
         "clip_timestamps": None,
         "hallucination_silence_threshold": None,
+        "hotwords": None
     }
 
     if asr_options is not None:
